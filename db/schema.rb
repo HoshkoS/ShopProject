65d34104961ee1ecefb7a9c029a065780198b077
HoshkoS <sofochka9500ho@gmail.com>
2023-05-27 13:46:25 +0300

    Add spec_helper and rails_helper files. Change column names to snake_case

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_161040) do
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "address"
-    t.string "phone"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "address", null: false
+    t.string "phone", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_161040) do
   create_table "product_orders", force: :cascade do |t|
     t.bigint "product_id", null: false
     t.bigint "order_id", null: false
-    t.integer "amount"
+    t.integer "amount", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_product_orders_on_order_id"
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:firstname) }
-    it { should validate_presence_of(:lastname) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:phone) }
   end
