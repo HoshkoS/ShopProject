b92ceb58f07dd9b5d9ec812c70d648208ea44d45
HoshkoS <sofochka9500ho@gmail.com>
2023-05-30 23:34:02 +0300

    Change numericality of price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Product, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
-    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_numericality_of(:balance).is_greater_than_or_equal_to(0) }
   end
 
