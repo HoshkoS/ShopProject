929056dacef2bb7a6fbbab91b4fa487797abe63e
HoshkoS <sofochka9500ho@gmail.com>
2023-05-30 22:59:27 +0300

    Fix commented problems

--- db/schema.rb
+++ db/schema.rb
@@ -34,10 +34,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_161040) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
-    t.decimal "price"
-    t.integer "balance"
+    t.decimal "price", precision: 8, scale: 2, default: "0.0", null: false
+    t.integer "balance", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:address) }
-    it { should validate_presence_of(:phone) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:address) }
+    it { is_expected.to validate_presence_of(:phone) }
   end
 
   describe 'associations' do
-    it { should have_many(:products) }
-    it { should have_many(:product_orders) }
+    it { is_expected.to have_many(:products) }
+    it { is_expected.to have_many(:product_orders) }
   end
 end
