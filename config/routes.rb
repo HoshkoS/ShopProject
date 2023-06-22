c5987df9af1a20c72745d2dc09aef2602849647d
HoshkoS <sofochka9500ho@gmail.com>
2023-06-22 20:18:16 +0300

    Change some carts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root "products#index"
 
+  resources :products, only: [:index, :show] do
+    member do
+      post :buy, to: "carts#update", as: "buy"
+      post :change_amount, to: "carts#update", as: "change_amount"
+      post :cancel_delivery, to: "carts#update", as: "cancel_delivery"
+    end
+  end
+
   get "cart", to: "carts#show", as: 'cart'
-  post "products/:id/buy", to: "carts#update", as: 'buy'
-  post "products/:id/change_amount", to: "carts#update", as: 'change_amount'
-  post "products/:id/cancel_delivery", to: "carts#update", as:'cancel_delivery'
   delete "clean_cart", to: "carts#destroy", as: "clean_cart"
 
   resources :orders
