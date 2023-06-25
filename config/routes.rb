35a28b1bb6dedf5396df814ecc998571b1318fe1
HoshkoS <sofochka9500ho@gmail.com>
2023-06-25 20:34:30 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,14 @@ Rails.application.routes.draw do
 
   resources :products do
     member do
-      resource :cart do
+      resource :cart, only: [] do
         [:add, :remove, :change_amount].each do |action|
-          patch action, to: "carts#update", as: "#{action}_product_in"
+          patch action, to: "carts#update", as: "#{action}_product_in", defaults: { update_action: action.to_s }
         end
       end
     end
   end
 
-  get "cart", to: "carts#show", as: 'cart_show'
-  delete "clean_cart", to: "carts#destroy", as: "clean_cart"
-
+  resource :cart
   resources :orders
 end
